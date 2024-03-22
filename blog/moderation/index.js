cf07cacdb602653c282219e8eb979515613cb456
baobao@gmail.com <@Baobao999>
2024-03-22 19:04:43 +0700

    UPDATE POST

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -5,8 +5,25 @@ const { default: axios } = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events',(req, res)=>{
+app.post('/events',async (req, res)=>{
 
+    const {type , data} = req.body;
+
+    if ( type == "CommentCreacted"){
+        const status = data.content.includes("orange") ? "rejected" : "approved";
+
+        await axios.post("http://localhost:4005/events",{
+            type:"CommentModerated",
+            data:{
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        });
+    }
+
+    res.send({});
 })
 
 
