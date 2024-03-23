808b50334fb6245515525f15331056aa03800fb5
baobao@gmail.com <@Baobao999>
2024-03-24 05:57:36 +0700

    update posts

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 const CommentList = ({comments}) => {
 
-    const renderComment = comments.map((item)=>(
-       <li key={item.id}>{item.content}</li>
-    ))
+    const renderComment = comments.map((item)=>{
+        let content;
+
+        if(item.status == "approved"){
+            content = item.content;
+        }
+
+        if(item.status == "pending"){
+            content = "This comment is awaiting moderation";
+        }
+
+        if(item.status == "rejected"){
+            content = "This comment has been rejected";
+        }
+
+       return(
+           <li key={item.id}>{content}</li>
+        )   
+    })
 
     return (
         <div>
--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -7,18 +7,26 @@ const app = express();
 app.use(bodyParser.json());
 // app.use(cors());
 
+const events = [];
+
 
 app.post("/events", (req, res) => {
 
-    const event = req.body
+    const event = req.body;
 
-    axios.post('http://localhost:4000/events', event)
-    axios.post('http://localhost:4001/events', event)
-    axios.post('http://localhost:4002/events', event)
-    axios.post('http://localhost:4003/events', event)
+    events.push(event);
+
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
 
   res.send({status:"OK"});
 });
+
+app.get("/events", (req , res)=>{
+  res.send(events)
+})
 
 app.listen(4005, () => {
   console.log("listening on 4005");
