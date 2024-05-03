7cbc22820011aa3810d2a8b39f07c3b459173c57
baobao@gmail.com <@Baobao999>
2024-05-03 17:51:25 +0700

    tiketting update: bad request err vs add mogo vs update signup BE

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -23,11 +25,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log("creacting a user...");
+    const exitingUser = await User.findOne({ email });
 
-    throw new DatabaseConnectionError();
+    if (exitingUser) {
+      throw new BadRequestError("Email is use");
+    }
 
-    res.status(201).send({ email, password });
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
